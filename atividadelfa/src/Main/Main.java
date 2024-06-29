82fdaba672789e7739c54c348b0ed376f91c946c
Rezende <128604858+PedroRezende14@users.noreply.github.com>
2024-06-29 19:37:14 -0300

    addTrantions

--- atividadelfa/src/Main/Main.java
+++ atividadelfa/src/Main/Main.java
@@ -14,30 +14,36 @@ public class Main {
 		            new EstadoImpl("q1"),
 		            new EstadoImpl("q2")
 		        ));
-		 Set<Character> alfabetoEntrada = new HashSet<>(Arrays.asList('a', 'b'));
+		 Set<Character> alfabetoEntrada = new HashSet<>(Arrays.asList('0', '1'));
 
 		 Set<Character> alfabetoPilha = new HashSet<>(Arrays.asList('Z', 'A'));
 
-		        // Definição do estado inicial
          Estado estadoInicial = new EstadoImpl("q0");
 
-		        // Definição dos estados finais
 		 Set<Estado> estadosFinais = new HashSet<>(Arrays.asList(
 		            new EstadoImpl("q2")
 		 ));
 
 		 AutomatoPilha automato = new AutomatoPilhaImpl(estados, alfabetoEntrada, alfabetoPilha, estadoInicial, estadosFinais);
 
-		        // Definição das transições
-		 automato.adicionarTransicao(new TransicaoImpl(new EstadoImpl("q0"), 'a', 'Z', new EstadoImpl("q1"), new char[] {'A', 'Z'}));
-		 automato.adicionarTransicao(new TransicaoImpl(new EstadoImpl("q1"), 'a', 'A', new EstadoImpl("q1"), new char[] {'A', 'A'}));
-		 automato.adicionarTransicao(new TransicaoImpl(new EstadoImpl("q1"), 'b', 'A', new EstadoImpl("q2"), new char[] {'ε'}));
-		 automato.adicionarTransicao(new TransicaoImpl(new EstadoImpl("q2"), 'b', 'A', new EstadoImpl("q2"), new char[] {'ε'}));
+		 automato.adicionarTransicao(new TransicaoImpl(new EstadoImpl("q0"), '0', 'Z', new EstadoImpl("q1"), new char[] {'A', 'Z'}));
+		 automato.adicionarTransicao(new TransicaoImpl(new EstadoImpl("q0"), '1', 'Z', new EstadoImpl("q1"), new char[] {'A', 'Z'}));
+		 
+		 automato.adicionarTransicao(new TransicaoImpl(new EstadoImpl("q1"), '1', 'A', new EstadoImpl("q1"), new char[] {'A', 'A'}));
+		 automato.adicionarTransicao(new TransicaoImpl(new EstadoImpl("q1"), '0', 'A', new EstadoImpl("q1"), new char[] {'A', 'A'}));
+		 
+		 automato.adicionarTransicao(new TransicaoImpl(new EstadoImpl("q1"), '1', 'A', new EstadoImpl("q2"), new char[] {'ε'}));
+		 automato.adicionarTransicao(new TransicaoImpl(new EstadoImpl("q1"), '0', 'A', new EstadoImpl("q2"), new char[] {'ε'}));
+		 automato.adicionarTransicao(new TransicaoImpl(new EstadoImpl("q2"), '1', 'A', new EstadoImpl("q2"), new char[] {'ε'}));
+		 automato.adicionarTransicao(new TransicaoImpl(new EstadoImpl("q2"), '0', 'A', new EstadoImpl("q2"), new char[] {'ε'}));
+		 
 		 automato.adicionarTransicao(new TransicaoImpl(new EstadoImpl("q2"), (char) 0, 'Z', new EstadoImpl("q2"), new char[] {'Z'}));
+		 automato.adicionarTransicao(new TransicaoImpl(new EstadoImpl("q1"), '0', 'Z', new EstadoImpl("q5"), new char[] {'ε'}));
+		 automato.adicionarTransicao(new TransicaoImpl(new EstadoImpl("q2"), '0', 'Z', new EstadoImpl("q5"), new char[] {'ε'}));
 
+		 System.out.println("Digite sua cadeia ou fim para finalizar o programa");
 		 String cadeia;
 		 do {
-			 System.out.println("Digite sua cadeia ou fim para finalizar o programa");
 			 Scanner entrada = new Scanner(System.in);
 			  cadeia = entrada.nextLine();
 			 if (automato.processarCadeia(cadeia)) {
