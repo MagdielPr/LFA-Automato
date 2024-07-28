d931d9f03fa9a0c58053b0734c63cc650e68fbea
MagdielPr <152613195+MagdielPr@users.noreply.github.com>
2024-07-28 20:30:29 -0300

    Delete LFA-Automato-main/atividadelfa directory

--- LFA-Automato-main/atividadelfa/src/interfac/AutomatoPilha.java
+++
@@ -1,8 +0,0 @@
-package Interface;
-
-public interface AutomatoPilha {
-    boolean processarCadeia(String cadeia, StringBuilder logger);
-    void adicionarTransicao(Transicao transicao);
-    void adicionarEstado(Estado estado);
-    void adicionarEstadoFinal(Estado estado);
-}
--- LFA-Automato-main/atividadelfa/src/interfac/Estado.java
+++
@@ -1,5 +0,0 @@
-package interfac;
-
-public interface Estado {
-    String getNome();
-}
