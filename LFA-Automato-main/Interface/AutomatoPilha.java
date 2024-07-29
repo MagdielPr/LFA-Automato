ff3f3fdf0cdddd0c5ee337c7a733d53d3fe0abf1
MagdielPr <152613195+MagdielPr@users.noreply.github.com>
2024-07-29 10:18:00 -0300

    Add files via upload

---
+++ LFA-Automato-main/Interface/AutomatoPilha.java
@@ -0,0 +1,8 @@
+package Interface;
+
+public interface AutomatoPilha {
+    boolean processarCadeia(String cadeia, StringBuilder logger);
+    void adicionarTransicao(Transicao transicao);
+    void adicionarEstado(Estado estado);
+    void adicionarEstadoFinal(Estado estado);
+}
